8e9e92d20acb0daaf791acb4b28a8d7b4e37339f
joaofx <joaofx@gmail.com>
2012-03-30 22:19:16 -0300

    Renamed to HumbleNetwork

---
+++ src/HumbleExample/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace HumbleExample
+{
+    using System;
+    using HumbleNetwork;
+    using HumbleNetwork.Streams;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var server = new HumbleServer();
+            server.AddCommand("echo", () => new EchoCommand());
+            server.Start(0);
+
+            var client = new HumbleClient();
+            client.Connect("localhost", server.Port);
+            client.Send("echo").Send("hello world");
+
+            Console.WriteLine("Client received: " + client.Receive());
+            Console.ReadKey();
+        }
+    }
+
+    public class EchoCommand : ICommand
+    {
+        public void Execute(IHumbleStream stream)
+        {
+            stream.Send(stream.Receive());
+        }
+    }
+}
