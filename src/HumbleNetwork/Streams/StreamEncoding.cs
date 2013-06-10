31da7c19dd02eea7f179724a32e8b3f61be0c3f8
joaofx <joaofx@gmail.com>
2013-06-10 12:46:36 -0300

    CI

---
+++ src/HumbleNetwork/Streams/StreamEncoding.cs
@@ -0,0 +1,46 @@
+namespace HumbleNetwork.Streams
+{
+    using System;
+    using System.Text;
+
+    public class StreamEncoding
+    {
+        private static Encoding Default
+        {
+            get
+            {
+                if (Type.GetType("Mono.Runtime") != null)
+                {
+                    return Encoding.UTF8;
+                }
+
+                return Encoding.Default;
+            }
+        }
+
+        public static string GetString(byte[] data)
+        {
+            return Default.GetString(data);
+        }
+
+        public static byte[] GetBytes(int data)
+        {
+            return BitConverter.GetBytes(data);
+        }
+
+        public static int GetInt32(byte[] data)
+        {
+            return BitConverter.ToInt32(data, 0);
+        }
+
+        public static string GetString(byte[] data, int index, int count)
+        {
+            return Default.GetString(data, index, count);
+        }
+
+        public static byte[] GetBytes(string delimiter)
+        {
+            return Default.GetBytes(delimiter);
+        }
+    }
+}
